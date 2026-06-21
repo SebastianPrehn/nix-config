8739f2a50aaccd00698efedcb40fcb0d732c5a54
Sebastian Larsen Prehn <slp@sebastianprehn.dk>
2026-06-21 12:00:59 +0200

    Update 2026-06-21. Remove librewolf for now, since it lacks latest updates

--- modules/hosts/odin/configuration.nix
+++ modules/hosts/odin/configuration.nix
@@ -88,8 +88,8 @@
     vial
     stash
     xivlauncher
-    bitwarden-desktop
     sabnzbd
+    whisparr
     suwayomi-server
     thunar
     thunar-archive-plugin
@@ -102,42 +102,11 @@
     fido2-manage
     unzip
     vagrant
+    cutter
   ];
 
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-
-  programs.firefox = {
-    enable = true;
-    package = pkgs.librewolf;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
-        "cookiebanners.service.mode" = 2; # Block cookie banners
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = true;
-        "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-      };
-      ExtensionSettings = {
-        "jid1-ZAdIEUB7XOzOJw@jetpack" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
-  };
-
-  environment.etc."firefox/policies/policies.json".target = "librewolf/policies/policies.json";
 
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
